7c96ddc0256c42a9b1cfc2dd1989171bd93c3521
root <root@Julio-i5-13600k>
2024-05-26 23:32:17 -0400

    feat: Implementando o teste que falida a função de criar um User

--- src/modules/user/useCases/createUserUseCase/createUserUseCase.spec.ts
+++ src/modules/user/useCases/createUserUseCase/createUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcrypt';
 import { UserRepositoryInMemory } from '../../repositories/UserRepositoryInMemory';
 import { CreateUserUseCase } from './createUserUseCase';
 
@@ -10,7 +11,32 @@ describe('Create User', () => {
     createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
   });
 
-  it('Deve ser capaz de criar um User', () => {
+  it('Deve ser capaz de criar um User', async () => {
     expect(userRepositoryInMemory.users).toEqual([]);
+
+    const user = await createUserUseCase.execute({
+      name: 'Teste',
+      email: 'teste@gmail.com',
+      password: 'Teste123',
+    });
+
+    expect(userRepositoryInMemory.users).toEqual([user]);
+  });
+
+  it('Deve ser capaz de criar um User com a senha encriptografada', async () => {
+    const userPasswordWithoutEncryption = 'Teste123';
+
+    const user = await createUserUseCase.execute({
+      name: 'Teste',
+      email: 'teste@gmail.com',
+      password: userPasswordWithoutEncryption,
+    });
+
+    const userHasPasswordEncrypted = await compare(
+      userPasswordWithoutEncryption,
+      user.password,
+    );
+
+    expect(userHasPasswordEncrypted).toBeTruthy();
   });
 });
